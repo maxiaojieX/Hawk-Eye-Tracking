402e0b1d03bc3c321be81b731a2a57324065ce96
xiaojie.Ma <1103173210@qq.com>
2018-11-16 14:47:41 +0800

    1116

--- src/main/java/com/xiaom/auth/Action.java
+++ src/main/java/com/xiaom/auth/Action.java
@@ -9,6 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class Action {
 
+    //http://lbsyun.baidu.com/index.php?title=yingyan/api/v3/trackupload
+
+    //纬度+经度
+    //http://api.map.baidu.com/geocoder/v2/?ak=0Rca0mKwmm0QSqLsEmIm0By4cB6GRsvG&callback=renderReverse&location=23.090788,113.271319&output=json&pois=1
+
+
     @GetMapping("/")
     public String ok() {
         return "index";
